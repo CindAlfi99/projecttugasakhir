e60c1ef352eefe053ebd8959e3eab95d5043e15d
CindAlfi99 <acindi38@gmail.com>
2021-04-16 12:42:47 +0700

    update files

---
+++ views/about.php
@@ -0,0 +1,65 @@
+<?php require 'template/header.php';?>
+
+
+    <div class="container">
+    <div class="row">
+    <div class="col-md-6 mt-5"><h2>Rumah Laundry 381</h2>
+    <p>Rumah Laundry 381 memiliki fitur dan sistem yang terintergrasi secara <i>online</i> dan sangat memprioritaskan kemudahan customers dalam menggunakan jasa dan fitur kami sepraktis mungkin. Serta berfokus pada kebutuhan dan kepuasaan pelanggan ketika menggunakan jasa Laundry kami.</p><br><p>
+   Rumah Laundry mulai merintis bisnis ini pada tahun 2017 dengan layanan pertama yaitu <i>Laundry</i> Kiloan dan satuan.</p><br><p>Pada tahun 2018 layanan Rumah Laundry 381 bertambah membuka layanan Laundry Karpet dan bedcover.</p><br>
+
+<p>Di tahun 2019 Rumah Laundry 381 mulai menambahkan lagi layanan Laundry Shoes yang  layanannya dikhususkan untuk membersihkan dan merawat sepatu customer sampai saat ini.</p><hr></div>
+    <div class="col-md-6"><img src="<?= BASE_URL;?>asset/img/logo.png" alt="">
+</div>
+    </div>
+    <!-- misi dan visi laundry -->
+    <div class="row mt-3">
+    <div class="col-md-12"><h2>Visi</h2>
+    "Menjadi perusahaan besar bidang jasa pencucian pakaian yang berorientasi pada kepuasan pelanggan."<br></div>
+    </div>
+    <div class="row mt-3">
+    <div class="col-md-11">
+    <h2>Misi</h2>
+    1. Menyediakan Layanan yang cepat serta teknologi yang praktis untuk kebutuhan  customer sehari-hari.<br>
+    2. Memberikan pengalaman terbaik demi mencapai Kepuasan Customer kami.<br>
+    3. Menjadi Brand yang bisa diandalkan bagi masyarakat indonesia.
+    </div>
+    </div>
+    <!-- batas -->
+    <!-- layanan laundry -->
+    <div class="row mt-5">
+    <div class="col-md-11 text-center">
+    <h2 class="text-center mb-3 mt-0 font-weight-bold">Layanan Rumah Laundry 381</h2>
+    <p>Rumah Laundry 381 Memiliki beberapa fitur layanan yang dapat memenuhi kebutuhan anda.</p>
+    </div></div>
+    <div class="row text-center">
+    <div class="col-md-2 kotak">
+    <img src="<?= BASE_URL; ?>asset/img/scale.png" class="img-fluid bg-primary rounded-circle" alt="">
+    <p>Laundry Kiloan</p><br>
+    <p>Layanan laundry pakaian yang proses dan transaksinya dilakukan dengan menghitung berat item laundry dari pakaian customer.</p><a href="<?= BASE_URL;?>views/layanan.php" class="btn btn-light">Detail</a></div>
+    <div class="col-md-3 kotak">
+    <img src="<?= BASE_URL; ?>asset/img/tshirt.png" class="img-fluid bg-primary rounded-circle" alt="">
+    <p>Laundry Satuan</p><br>
+    <p>Layanan laundry pakaian yang proses dan transaksinya dilakukan dengan melihat jenis dan jumlah dari pakaian customer.</p><a href="<?= BASE_URL;?>views/layanan.php" class="btn btn-light">Detail</a></div>
+    <div class="col-md-3 kotak">
+    <img src="<?= BASE_URL; ?>asset/img/carpet.png" class="img-fluid bg-primary rounded-circle" alt="">
+    <p>Laundry Karpet</p><br>
+    <p>Layanan laundry Karpet diproses dengan menggunakan mesin khusus sehingga bisa dipastikan kebersihannya.</p><a href="<?= BASE_URL;?>views/layanan.php" class="btn btn-light">Detail</a></div>
+    <div class="col-md-2 kotak">
+    <img src="<?= BASE_URL; ?>asset/img/running.png" class="img-fluid bg-primary rounded-circle" alt="">
+    <p>Laundry Shoes</p><br>
+    <p>Laundry Shoes merupakan jenis pelayanan laundry yang dikhususkan untuk kebersihan dan perawatan Sepatu.</p><a href="<?= BASE_URL;?>views/layanan.php" class="btn btn-light">Detail</a></div>
+    <div class="col-md-2 kotak">
+    <img src="<?= BASE_URL; ?>asset/img/cover.png" class="img-fluid bg-primary rounded-circle" alt="">
+    <p>Laundry Bed Cover</p><br>
+    <p>Jangan paksakan diri untuk mencuci sendiri bed cover Anda. Laundry Bed Cover merupakan salah satu layanan favorit pelanggan Kami. Bersih, rapih, dan wangi.</p><a href="<?= BASE_URL;?>views/layanan.php" class="btn btn-light">Detail</a></div>
+
+    </div>
+    <!-- batas -->
+
+    </div>
+    <?php require 'template/footer.php';?>
+
+</body>
+</html>
+
+  
---
+++ views/template/footer.php
@@ -0,0 +1,22 @@
+<footer class="footer bg-primary">
+<div class="row mt-5">
+<div class="col-md-5"><img src="<?=BASE_URL;?>asset/img/logo.png" alt="" width="350" height="350"></div>
+<div class="col-md-4 mt-5"><h3>Contact us</h3>
+<p class="mt-5">Jl. May Zen, Sei Selincah Kec. Kalidoni,<br> Kota Palembang</p>
+<br><br>
+<img src="<?= BASE_URL;?>asset/img/phone-call.png" width="25" height="25" alt="">   0811-437-223
+<br><br>
+<img src="<?= BASE_URL;?>asset/img/email.png" width="25" height="25" alt="">   acindi38@gmail.com
+</div>
+<div class="col-md-3 mt-5 layanan"><h3>Layanan</h3>
+<p class="mt-5"><img src="<?= BASE_URL;?>asset/img/scale.png" width="25" height="25"><a href="#">Laundry Kiloan</a></p>
+<p><img src="<?= BASE_URL;?>asset/img/tshirt.png" width="25" height="25"><a href="#">Laundry Satuan</a></p>
+<p><img src="<?= BASE_URL;?>asset/img/carpet.png" width="25" height="25"><a href="#">Laundry Karpet</a></p>
+<p><img src="<?= BASE_URL;?>asset/img/running.png" width="25" height="25"><a href="#">Laundry Sepatu</a></p>
+<p><img src="<?= BASE_URL;?>asset/img/cover.png" width="25" height="25"><a href="#">Laundry Bed Cover</a></p></div></div>
+</footer>
+<script src="<?= BASE_URL; ?>asset/js/jquery.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="<?= BASE_URL; ?>asset/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+
+</body>
+</html>
