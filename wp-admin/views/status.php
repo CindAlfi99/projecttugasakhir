225f1de0eeeb1bc3a7b52758d8591ab9b73343c8
Zia <zialdiansyah@vutura.io>
2021-04-27 21:44:03 +0700

    ubah status laundry

--- wp-admin/views/status.php
+++ wp-admin/views/status.php
@@ -4,7 +4,9 @@ $id = $_GET['id'];
 $status = $_GET['status'];
 if(isset($id)){
     $query =mysqli_query($connection, "UPDATE order_masuk SET status='$status' WHERE id=$id");
-   
+    if(!$query ) {
+        die('Invalid query: ' . mysqli_error($connection));
+    }
     header("Location: order.php");
-
+    exit;
 }
