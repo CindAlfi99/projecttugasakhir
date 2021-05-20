e145aaab94e5ab79eb58d1b70e65cccd0e7d0b44
CindAlfi99 <acindi38@gmail.com>
2021-05-20 19:03:47 +0700

    update

--- helper.php
+++ helper.php
@@ -1,6 +1,7 @@
 <?php
 require 'config/config.php';
-$no_resi=0;
+require __DIR__ . '/vendor/autoload.php';
+
 $query = mysqli_query($conn, "SELECT order_masuk.jenis_item, order_masuk.jumlah, order_masuk.ongkir,layanan.jenis_item, layanan.satuan, layanan.harga FROM order_masuk JOIN layanan ON order_masuk.jenis_item = layanan.jenis_item");
 $join_tbl = mysqli_fetch_assoc($query);
 if ($_GET["jenis"]) {
@@ -29,6 +30,18 @@ if ($_GET["jenis"]) {
             </select>";
     }
 } else {
+    $options = array(
+        'cluster' => 'ap1',
+        'useTLS' => true
+      );
+      $pusher = new Pusher\Pusher(
+        'ee0a2d4a7d1a6c8ef4f8',
+        'cadca09b4c0acaaeaaed',
+        '1206705',
+        $options
+      );
+    
+     
     $val = $_POST;
     $length = count($_POST['layanan']);
     $no_resi = rand(1000, 9999);
@@ -47,11 +60,16 @@ if ($_GET["jenis"]) {
     $total_byr = $join_tbl['harga'] + $ongkir * $join_tbl['jumlah'] - 10000 ;
    
     for ($i=0; $i < $length; $i++) { 
+ 
         $sql="INSERT INTO order_masuk (no_resi, nama_pemesan, no_wa, alamat_jemput, jenis_layanan, jenis_item, jumlah,satuan, harga,ongkir,total_bayar, status_cucian,status_pembayaran,mode, tanggal_pesan, tanggal_selesai)";
         $sql.=' VALUES ("'.$no_resi.'", "'.$nama.'", "'.$no_wa.'", "'.$alamat.'", "'.$layanan[$i]['jenis'].'", "'.$layanan[$i]['item'].'", '.$layanan[$i]['jml_item'].',"'.$satuan.'","'.$harga.'","'.$ongkir.'","'.$total_byr.'", "'.$status.'", "'.$status_pembayaran.'", "'.$mode.'","'.$tgl_pesan.'", "'.$tgl_selesai.'")';
         $result = mysqli_query($conn, $sql);
         if (!$result) {
             die('Invalid query: ' . mysqli_error($conn));
+        }
+        elseif($result){
+            $data['message'] = '<b>Pesanan Baru Masuk</b>';
+            $pusher->trigger('my-channel', 'my-event', $data);
         }
     }
     header('Content-Type: application/json');
